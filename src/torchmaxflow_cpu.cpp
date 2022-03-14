b9bc794e7e699d4932f946dacfe946dc2f27f39d
masadcv <muhammad.asad@kcl.ac.uk>
2022-03-14 14:06:19 +0000

    fix windows build errors, refactor code a bit
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- src/torchmaxflow_cpu.cpp
+++ src/torchmaxflow_cpu.cpp
@@ -51,6 +51,17 @@ float l2distance(const float *in1, const float *in2, int size)
     return std::sqrt(ret_sum);
 }
 
+float l2distance(const std::vector<float> &in1, const std::vector<float> &in2)
+{
+    int size = in1.size();
+    float ret_sum = 0.0;
+    for (int c_i = 0; c_i < size; c_i++)
+    {
+        ret_sum += (in1[c_i] - in2[c_i]) * (in1[c_i] - in2[c_i]);
+    }
+    return std::sqrt(ret_sum);
+}
+
 torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &prob, const float &lambda, const float &sigma)
 {
     // get input dimensions
@@ -77,13 +88,16 @@ torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &pro
 
     // define max weight value
     float max_weight = -100000;
+    float pval, qval, l2dis, n_weight, s_weight, t_weight, prob_bg, prob_fg;
+    int pIndex, qIndex;
+    std::vector<float> pval_v(channel);
+    std::vector<float> qval_v(channel);
 
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
         {
-            float pval;
-            float pval_v[channel];
+
             if (channel == 1)
             {
                 pval = image_ptr[0][0][h][w];
@@ -95,10 +109,7 @@ torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                     pval_v[c_i] = image_ptr[0][c_i][h][w];
                 }
             }
-            float l2dis, n_weight;
-            int pIndex = h * width + w;
-            int qIndex;
-
+            pIndex = h * width + w;
             for (int i = 0; i < 2; i++)
             {
                 const int hn = h + Xoff[i];
@@ -106,8 +117,6 @@ torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                 if (hn < 0 || wn < 0)
                     continue;
 
-                float qval;
-                float qval_v[channel];
                 if (channel == 1)
                 {
                     qval = image_ptr[0][0][hn][wn];
@@ -119,7 +128,7 @@ torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                     {
                         qval_v[c_i] = image_ptr[0][c_i][hn][wn];
                     }
-                    l2dis = l2distance(pval_v, qval_v, channel);
+                    l2dis = l2distance(pval_v, qval_v);
                 }
                 n_weight = lambda * exp(-(l2dis * l2dis) / (2 * sigma * sigma));
                 qIndex = hn * width + wn;
@@ -138,16 +147,14 @@ torch::Tensor maxflow2d_cpu(const torch::Tensor &image, const torch::Tensor &pro
     {
         for (int w = 0; w < width; w++)
         {
-            float s_weight, t_weight;
 
-            float prob_bg, prob_fg;
             // avoid log(0)
             prob_bg = std::max(prob_ptr[0][0][h][w], std::numeric_limits<float>::epsilon());
             prob_fg = std::max(prob_ptr[0][1][h][w], std::numeric_limits<float>::epsilon());
             s_weight = -log(prob_bg);
             t_weight = -log(prob_fg);
 
-            int pIndex = h * width + w;
+            pIndex = h * width + w;
             g->add_tweights(pIndex, s_weight, t_weight);
         }
     }
@@ -195,6 +202,10 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
 
     // define max weight value
     float max_weight = -100000;
+    float pval, qval, l2dis, n_weight, s_weight, t_weight, prob_bg, prob_fg;
+    int pIndex, qIndex;
+    std::vector<float> pval_v(channel);
+    std::vector<float> qval_v(channel);
 
     for (int d = 0; d < depth; d++)
     {
@@ -202,8 +213,6 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
         {
             for (int w = 0; w < width; w++)
             {
-                float pval;
-                float pval_v[channel];
                 if (channel == 1)
                 {
                     pval = image_ptr[0][0][d][h][w];
@@ -215,9 +224,7 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                         pval_v[c_i] = image_ptr[0][c_i][d][h][w];
                     }
                 }
-                float l2dis, n_weight;
-                int pIndex = d * height * width + h * width + w;
-                int qIndex;
+                pIndex = d * height * width + h * width + w;
 
                 for (int i = 0; i < 3; i++)
                 {
@@ -227,8 +234,6 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                     if (dn < 0 || hn < 0 || wn < 0)
                         continue;
 
-                    float qval;
-                    float qval_v[channel];
                     if (channel == 1)
                     {
                         qval = image_ptr[0][0][dn][hn][wn];
@@ -240,7 +245,7 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
                         {
                             qval_v[c_i] = image_ptr[0][c_i][dn][hn][wn];
                         }
-                        l2dis = l2distance(pval_v, qval_v, channel);
+                        l2dis = l2distance(pval_v, qval_v);
                     }
                     n_weight = lambda * exp(-(l2dis * l2dis) / (2 * sigma * sigma));
                     qIndex = dn * height * width + hn * width + wn;
@@ -262,16 +267,13 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
         {
             for (int w = 0; w < width; w++)
             {
-                float s_weight, t_weight;
-
-                float prob_bg, prob_fg;
                 // avoid log(0)
                 prob_bg = std::max(prob_ptr[0][0][d][h][w], std::numeric_limits<float>::epsilon());
                 prob_fg = std::max(prob_ptr[0][1][d][h][w], std::numeric_limits<float>::epsilon());
                 s_weight = -log(prob_bg);
                 t_weight = -log(prob_fg);
 
-                int pIndex = d * height * width + h * width + w;
+                pIndex = d * height * width + h * width + w;
                 g->add_tweights(pIndex, s_weight, t_weight);
             }
         }
@@ -298,65 +300,68 @@ torch::Tensor maxflow3d_cpu(const torch::Tensor &image, const torch::Tensor &pro
 void add_interactive_seeds(torch::Tensor &prob, const torch::Tensor &seed, const int &num_dims)
 {
     // implements Equation 7 from:
-    //  Wang, Guotai, et al. 
-    //  "Interactive medical image segmentation using deep learning with image-specific fine tuning." 
+    //  Wang, Guotai, et al.
+    //  "Interactive medical image segmentation using deep learning with image-specific fine tuning."
     //  IEEE TMI (2018).
-    
+
     const int channel = prob.size(1);
-    if(num_dims == 4) // 2D
+    if (num_dims == 4) // 2D
     {
         const int height = prob.size(2);
         const int width = prob.size(3);
-        
+
         auto prob_ptr = prob.accessor<float, 4>();
         auto seed_ptr = seed.accessor<float, 4>();
 
-        for (int h=0; h < height; h++)
+        for (int h = 0; h < height; h++)
         {
-            for (int w=0; w<width; w++)
+            for (int w = 0; w < width; w++)
             {
-                if(seed_ptr[0][0][h][w] > 0)
+                if (seed_ptr[0][0][h][w] > 0)
                 {
                     prob_ptr[0][0][h][w] = 1.0;
                     prob_ptr[0][1][h][w] = 0.0;
-
                 }
                 else if (seed_ptr[0][1][h][w] > 0)
                 {
                     prob_ptr[0][0][h][w] = 0.0;
                     prob_ptr[0][1][h][w] = 1.0;
-
+                }
+                else
+                {
+                    continue;
                 }
             }
         }
-
     }
-    else if(num_dims == 5) // 3D
+    else if (num_dims == 5) // 3D
     {
         const int depth = prob.size(2);
         const int height = prob.size(3);
         const int width = prob.size(4);
-        
+
         auto prob_ptr = prob.accessor<float, 5>();
         auto seed_ptr = seed.accessor<float, 5>();
-        
-        for (int d=0; d < depth; d++)
+
+        for (int d = 0; d < depth; d++)
         {
-            for (int h=0; h < height; h++)
+            for (int h = 0; h < height; h++)
             {
-                for (int w=0; w<width; w++)
+                for (int w = 0; w < width; w++)
                 {
-                    if(seed_ptr[0][0][d][h][w] > 0)
+                    if (seed_ptr[0][0][d][h][w] > 0)
                     {
                         prob_ptr[0][0][d][h][w] = 1.0;
                         prob_ptr[0][1][d][h][w] = 0.0;
-
                     }
                     else if (seed_ptr[0][1][d][h][w] > 0)
                     {
                         prob_ptr[0][0][d][h][w] = 0.0;
                         prob_ptr[0][1][d][h][w] = 1.0;
-
+                    }
+                    else
+                    {
+                        continue;
                     }
                 }
             }

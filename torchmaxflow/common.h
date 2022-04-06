26bdf7b0765c14c2a67348f8538bbb5ee6e15c57
masadcv <muhammad.asad@kcl.ac.uk>
2022-04-06 14:01:52 +0100

    improve input checking, factor out code in common
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- torchmaxflow/common.h
+++ torchmaxflow/common.h
@@ -20,7 +20,79 @@ void print_shape(const torch::Tensor &data)
     }
 }
 
+void check_shape_match(const torch::Tensor &in1, const torch::Tensor &in2, const int &dims)
+{
+    if (in1.dim() != in2.dim())
+    {
+        throw std::runtime_error("dimensions of input tensors do not match " 
+            + std::to_string(in1.dim() - 2) + " vs " + std::to_string(in2.dim() - 2));
+    }
 
-// #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor.")
-// #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous.")
-// #define CHECK_CONTIGUOUS_CUDA(x) CHECK_CUDA(x);CHECK_CONTIGUOUS(x)
+    for(int i=0; i < dims; i++)
+    {
+        if(in1.size(2+i) != in2.size(2+i))
+        {
+            std::cout << "Tensor1 ";
+            print_shape(in1);
+            std::cout << "Tensor2 ";
+            print_shape(in2);
+            throw std::runtime_error("shapes of input tensors do not match");
+        }
+    }
+}
+
+void check_cpu(const torch::Tensor &in)
+{
+    if (in.is_cuda())
+    {
+        throw std::runtime_error("Library currently does not support CUDA, please pass CPU tensors as mytensor.cpu().");
+    }
+}
+
+void check_single_batch(const torch::Tensor &in)
+{
+    if (in.size(0) != 1)
+    {
+        throw std::runtime_error("Library currently only supports single batch input.");
+    }
+}
+
+void check_binary_channels(const torch::Tensor &in)
+{
+    if (in.size(1) != 2)
+    {
+        throw std::runtime_error("Library currently only supports binary probability.");
+    }
+}
+
+void check_input_maxflow(const torch::Tensor &image, const torch::Tensor &prob, const int &num_dims)
+{
+    // check input dimensions
+    check_cpu(image);
+    check_cpu(prob);
+    
+    check_single_batch(image);
+    check_single_batch(prob);
+
+    check_binary_channels(prob);
+
+    check_shape_match(image, prob, num_dims-2);    
+}
+
+void check_input_maxflow_interactive(const torch::Tensor &image, const torch::Tensor &prob, const torch::Tensor &seed, const int &num_dims)
+{
+    // check input dimensions
+    check_cpu(image);
+    check_cpu(prob);
+    check_cpu(seed);
+    
+    check_single_batch(image);
+    check_single_batch(prob);
+    check_single_batch(seed);
+
+    check_binary_channels(prob);
+    check_binary_channels(seed);
+
+    check_shape_match(image, prob, num_dims-2);    
+    check_shape_match(image, seed, num_dims-2);    
+}
--- torchmaxflow/torchmaxflow.cpp
+++ torchmaxflow/torchmaxflow.cpp
@@ -3,61 +3,28 @@
 #include "torchmaxflow.h"
 #include "common.h"
 
-void check_shape_match(const torch::Tensor &in1, const torch::Tensor &in2, const int &dims)
-{
-    for(int i=0; i < dims; i++)
-    {
-        if(in1.size(2+i) != in2.size(2+i))
-        {
-            std::cout << "Tensor1 ";
-            print_shape(in1);
-            std::cout << "Tensor2 ";
-            print_shape(in2);
-            AT_ERROR("Error: shapes of input tensors do not match");
-        }
-    }
-}
-
 torch::Tensor maxflow(const torch::Tensor &image, const torch::Tensor &prob, const float &lambda, const float &sigma)
 {
-    // check input dimensions
     // could be 2D or 3D tensors of shapes
     // 2D: 1 x C x H x W  (4 dims)
     // 3D: 1 x C x D x H x W (5 dims)
     const int num_dims = prob.dim();
-    if (num_dims != 4 && num_dims != 5)
-    {
-        throw std::runtime_error(
-            "function only supports 2D or 3D spatial inputs, received " + std::to_string(num_dims - 2));
-    }
-
-    if (image.is_cuda() || prob.is_cuda())
-    {
-        AT_ERROR("Library currently does not support CUDA, please pass CPU tensors as mytensor.cpu().");
-    }
-
-    if (image.size(0) != 1 || prob.size(0) != 1)
-    {
-        AT_ERROR("Library currently only supports single batch input.");
-    }
-
-    if (prob.size(1) != 2)
-    {
-        AT_ERROR("Library currently only supports binary probability.");
-    }
+    check_input_maxflow(image, prob, num_dims);
 
     // 2D case: 1 x C x H x W
     if (num_dims == 4)
     {
-        check_shape_match(image, prob, 2);
         return maxflow2d_cpu(image, prob, lambda, sigma);
     }
-
     // 3D case: 1 x C x D x H x W
     else if (num_dims == 5)
     {
-        check_shape_match(image, prob, 3);
         return maxflow3d_cpu(image, prob, lambda, sigma);
+    }
+    else
+    {
+        throw std::runtime_error(
+            "Library only supports 2D or 3D spatial inputs, received " + std::to_string(num_dims - 2) + "D inputs");
     }
 }
 
@@ -68,31 +35,7 @@ torch::Tensor maxflow_interactive(const torch::Tensor &image, torch::Tensor &pro
     // 2D: 1 x C x H x W  (4 dims)
     // 3D: 1 x C x D x H x W (5 dims)
     const int num_dims = prob.dim();
-    if (num_dims != 4 && num_dims != 5)
-    {
-        throw std::runtime_error(
-            "function only supports 2D or 3D spatial inputs, received " + std::to_string(num_dims - 2));
-    }
-
-    if (image.is_cuda() || prob.is_cuda() || seed.is_cuda())
-    {
-        AT_ERROR("Library currently does not support CUDA, please pass CPU tensors as mytensor.cpu().");
-    }
-
-    if (image.size(0) != 1 || prob.size(0) != 1 || seed.size(0) != 1)
-    {
-        AT_ERROR("Library currently only supports single batch input.");
-    }
-
-    if (prob.size(1) != 2)
-    {
-        AT_ERROR("Library currently only supports binary probability.");
-    }
-
-    if (seed.size(1) != 2)
-    {
-        AT_ERROR("Library currently only supports binary seeds.");
-    }
+    check_input_maxflow_interactive(image, prob, seed, num_dims);
 
     // add interactive points to prob using seed locations
     add_interactive_seeds(prob, seed, num_dims);
@@ -100,16 +43,17 @@ torch::Tensor maxflow_interactive(const torch::Tensor &image, torch::Tensor &pro
     // 2D case: 1 x C x H x W
     if (num_dims == 4)
     {
-        check_shape_match(image, prob, 2);
-        check_shape_match(image, seed, 2);
         return maxflow2d_cpu(image, prob, lambda, sigma);
     }
     // 3D case: 1 x C x D x H x W
+    else if (num_dims == 5)
+    {
+        return maxflow3d_cpu(image, prob, lambda, sigma);
+    }
     else
     {
-        check_shape_match(image, prob, 3);
-        check_shape_match(image, seed, 3);
-        return maxflow3d_cpu(image, prob, lambda, sigma);
+        throw std::runtime_error(
+            "Library only supports 2D or 3D spatial inputs, received " + std::to_string(num_dims - 2) + "D inputs");
     }
 }
 

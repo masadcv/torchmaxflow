3b3e10bf7c753415b66a914b1587d7940c69cbe8
masadcv <muhammad.asad@kcl.ac.uk>
2022-04-06 17:51:51 +0100

    rc2 version

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ with open("README.md", "r") as fh:
 print(get_extensions())
 setup(
     name='torchmaxflow',
-    version="0.0.3rc1",
+    version="0.0.3rc2",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
